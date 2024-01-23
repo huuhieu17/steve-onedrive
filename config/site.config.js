53b826ef2327d4f93896cc9e287ef9342d7bf0ce
Nguyen Huu Hieu <31349335+huuhieu17@users.noreply.github.com>
2024-01-23 11:18:06 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "lyc8503's OneDrive",
+  title: "Steve's OneDrive",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: '/',
@@ -51,7 +51,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/lyc8503',
+      link: 'https://github.com/huuhieu17',
     },
   ],
 
